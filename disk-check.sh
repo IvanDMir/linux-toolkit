305d916e8c17c54c95f32b2968ac60047e696b39
Ivan Mir <ivan.mir444@gmail.com>
2026-09-06 04:45:06 +0000

    Umbral a 75

--- disk-check.sh
+++ disk-check.sh
@@ -7,7 +7,7 @@ usado=$(df -h / | awk 'NR==2 {print $3}' )
 disponible=$(df -h / | awk 'NR==2 {print $4}' )
 
 echo "El espacio disponible es $disponible y el espacio usado es $usado" >>log.txt
-if [ "$uso" -gt 80 ]; then
+if [ "$uso" -gt 75 ]; then
     echo " $(date) - el porcentaje es mayor a 80%" >> log.txt
 else
     echo "$(date) - Uso disponible" >> log.txt
