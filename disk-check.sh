f3b1aac710bfaf2c6c46338f1af3b8b24c91825a
Ivan Mir <ivan.mir444@gmail.com>
2026-09-06 02:52:09 +0000

    Cambio de formatio de fecha

--- disk-check.sh
+++ disk-check.sh
@@ -2,8 +2,15 @@
 
 uso=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
 
+usado=$(df -h / | awk 'NR==2 {print $3}' )
+
+disponible=$(df -h / | awk 'NR==2 {print $4}' )
+
+echo "El espacio disponible es $disponible y el espacio usado es $usado" >>log.txt
 if [ "$uso" -gt 80 ]; then
-    echo "el porcentaje es mayor a 80%"
+    echo " $(date) - el porcentaje es mayor a 80%" >> log.txt
 else
-    echo "Uso disponible"
+    echo "$(date) - Uso disponible" >> log.txt
 fi
+
+
